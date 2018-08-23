5591b6e513cd377669d33a268caad437f4a069ad
stanislaspiron <39823762+stanislaspiron@users.noreply.github.com>
2018-08-23 14:43:50 +0200

    Update LTM_oracle_tns_rewrite.tcl

--- LTM_oracle_tns_rewrite.tcl
+++ LTM_oracle_tns_rewrite.tcl
@@ -18,15 +18,7 @@ when SERVER_DATA {
 		[set new_redirect_data [string map "(HOST=$server_ip) (HOST=$virtual_ip)" $redirect_data]] ne $redirect_data } {
 			set new_redirect_length [string length $new_redirect_data]
 			set collected_length [expr {$collected_length + [TCP::payload length]}]
-			set redirect_port [findstr $redirect_data "(PORT=" 6 ")"]
 			TCP::payload replace 0 [TCP::payload length] [binary format SSccSSA* [expr {$new_redirect_length+10}] $packet_checksum $type $reserved_byte $header_checksum $new_redirect_length $new_redirect_data]
-			listen {
-				proto 6
-				timeout 30
-				bind $client_vlan $virtual_ip $redirect_port
-				server $server_ip $redirect_port
-				allow $client_ip
-			}
 	}
 
 	TCP::release
